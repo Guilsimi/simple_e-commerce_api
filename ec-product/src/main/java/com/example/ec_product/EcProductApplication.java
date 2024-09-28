8a7ed383d5cf23ea4a8a906a7f98765edbee0651
Guilsimi <guilherme.simioni04@gmail.com>
2024-09-28 12:38:17 -0400

    Creating a product microsservice

---
+++ ec-product/src/main/java/com/example/ec_product/EcProductApplication.java
@@ -0,0 +1,13 @@
+package com.example.ec_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcProductApplication.class, args);
+	}
+
+}
---
+++ ec-product/src/test/java/com/example/ec_product/EcProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ec_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
