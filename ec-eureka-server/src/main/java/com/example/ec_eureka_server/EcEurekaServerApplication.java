5cdd91cd9797010ce8207818ac3f2060708e56c8
Guilsimi <guilherme.simioni04@gmail.com>
2024-10-08 10:59:53 -0400

    Eureka server configurations

---
+++ ec-eureka-server/src/main/java/com/example/ec_eureka_server/EcEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.ec_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EcEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcEurekaServerApplication.class, args);
+	}
+
+}
