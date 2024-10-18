9a9d6ed986a247c42067a54efb6866e8627b68a9
Guilsimi <guilherme.simioni04@gmail.com>
2024-10-18 17:44:41 -0400

    Private Repository configs

--- ec-order/src/main/java/com/example/ec_order/resources/GetOrderResources.java
+++ ec-order/src/main/java/com/example/ec_order/resources/GetOrderResources.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.example.ec_order.services.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.annotation.Resource;
 
+@RefreshScope
 @Resource
 @RestController
 @RequestMapping(value = "/orders")
