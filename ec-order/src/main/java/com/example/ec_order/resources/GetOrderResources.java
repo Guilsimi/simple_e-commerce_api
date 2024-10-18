56f1b544097a531bf1e78b6c4419d96eafffac60
Guilsimi <guilherme.simioni04@gmail.com>
2024-10-18 16:58:33 -0400

    turning Order into a config server client

--- ec-order/src/main/java/com/example/ec_order/resources/GetOrderResources.java
+++ ec-order/src/main/java/com/example/ec_order/resources/GetOrderResources.java
@@ -3,7 +3,10 @@ package com.example.ec_order.resources;
 import java.util.Date;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,11 +26,21 @@ import jakarta.annotation.Resource;
 @RequestMapping(value = "/orders")
 public class GetOrderResources {
 
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     OrderService orderService;
 
     @Autowired
     GetOrderAndProducts getOrderAndProducts;
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs() {
+        Logger logger = LoggerFactory.getLogger(GetOrderResources.class);
+        logger.info(testConfig);
+        return ResponseEntity.noContent().build();
+    }
 
     @GetMapping
     public ResponseEntity<List<Order>> findAll() {
